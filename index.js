a37f3f51e1bcde0a65fd8f242ccae30e5916799e
PanRagon <thebigecko@gmail.com>
2019-10-14 20:14:43 +0200

    Started project & created some HTML

---
+++ index.js
@@ -0,0 +1,52 @@
+var html = document.getElementById("html");
+var btn = document.getElementById("battletagBtn");
+
+btn.addEventListener("click", function() {
+    var picksCount = document.getElementById("picksCount").value;
+    var myBattletag = document.getElementById("myBattletag").value;
+    var enemyBattletag = document.getElementById("enemyBattletag").value;
+    var bansCount = document.getElementById("bansCount").value;
+    renderPicksHTML(myBattletag, enemyBattletag, picksCount, bansCount)
+})
+
+function renderPicksHTML(myBattletag, enemyBattletag, picks, bans) {
+    html.innerHTML = `
+    <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Picks</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+    <div id="mainPageWrapper">
+        <div id="leftScreen">
+            <h1>${myBattletag}</h1>
+            <h3>Pick ${picks} heroes</h3>
+            <image id="friendlyDruid" class="classLogo"src="images/druid.png"></image>
+            <image id="friendlyHunter" class="classLogo" src="images/hunter.png"></image>
+            <image id="friendlyMage" class="classLogo" src="images/mage.png"></image>
+            <image id="friendlyPaladin" class="classLogo" src="images/paladin.png"></image>
+            <image id="friendlyPriest" class="classLogo" src="images/priest.png"></image>
+            <image id="friendlyRogue" class="classLogo" src="images/rogue.png"></image>
+            <image id="friendlyShaman" class="classLogo" src="images/shaman.png"></image>
+            <image id="friendlyWarlock" class="classLogo" src="images/warlock.png"></image>
+            <image id="friendlyWarrior" class="classLogo" src="images/warrior.png"></image>
+        </div>
+        <div id="rightScreen">
+            <h1>${enemyBattletag}</h1>
+            <h3>Pick ${picks} heroes</h3>
+            <image id="enemyDruid" class="classLogo"src="images/druid.png"></image>
+            <image id="enemyHunter" class="classLogo" src="images/hunter.png"></image>
+            <image id="enemyMage" class="classLogo" src="images/mage.png"></image>
+            <image id="enemyPaladin" class="classLogo" src="images/paladin.png"></image>
+            <image id="enemyPriest" class="classLogo" src="images/priest.png"></image>
+            <image id="enemyRogue" class="classLogo" src="images/rogue.png"></image>
+            <image id="enemyShaman" class="classLogo" src="images/shaman.png"></image>
+            <image id="enemyWarlock" class="classLogo" src="images/warlock.png"></image>
+            <image id="enemyWarrior" class="classLogo" src="images/warrior.png"></image>
+        </div>
+    </div>
+    <script src="index.js"></script>
+</body>`
+}
